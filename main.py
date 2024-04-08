2cb1419fc41bdd7abaa8333ecd651587b8bb83a1
vjern <jocelyn.vernay@gmail.com>
2024-04-08 17:06:40 +0200

    Add draft

---
+++ main.py
@@ -0,0 +1,57 @@
+import sys
+import re
+import json
+import argparse
+from typing import Iterable
+
+
+def process(data: Iterable[str]) -> Iterable[str]:
+    for i, row in enumerate(data):
+        row = row.strip()
+        if not i % 5:
+            yield 'Multiple de 5'
+        elif '$' in row:
+            yield re.sub(r'\s', '_', row)
+        elif row.endswith('.'):
+            yield row
+        elif row.startswith('{'):
+            decoded = json.loads(row)
+            decoded['pair'] = not i % 2
+            yield json.dumps(decoded)
+        else:
+            yield 'Rien à afficher'
+
+
+def main(argv: list[str] = sys.argv[1:]):
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath')
+
+    args = parser.parse_args(argv)
+
+    with open(args.filepath) as f:
+        data = process(f)
+        for row in data:
+            print(row)
+
+
+def test_rules():
+    assert list(process([
+        'Line dot.',
+        'Line dot.',
+        json.dumps({"a": 3}),
+        json.dumps({"b": 4}),
+        f'The price is $$$\ttoo expensive?',
+        'no condition'
+    ])) == [
+        'Multiple de 5',
+        'Line dot.',
+        json.dumps({"a": 3, "pair": True}),
+        json.dumps({"b": 4, "pair": False}),
+        'The_price_is_$$$_too_expensive?',
+        'Multiple de 5'
+    ]
+
+
+if __name__ == '__main__':
+    main()
